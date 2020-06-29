471e3c81885d831c7aab61f4daa6ca7a0baddc33
Signs Madueke <signsmadueke1@gmail.com>
2020-06-29 21:07:04 +0100

    ...

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -24,11 +24,11 @@ if ( $related_products ) : ?>
 	<section class="related products">
 
 		<?php
-		$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related products', 'woocommerce' ) );
+		$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related Books', 'woocommerce' ) );
 
 		if ( $heading ) :
 			?>
-			<h2><?php echo esc_html( $heading ); ?></h2>
+			<h1 class="lined-heading"><?php echo esc_html( $heading ); ?></h1>
 		<?php endif; ?>
 		
 		<?php woocommerce_product_loop_start(); ?>
