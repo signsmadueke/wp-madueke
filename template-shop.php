acf7c084b49d8b826d715d33641fa24ecf3c5b25
Signs Madueke <signsmadueke1@gmail.com>
2020-07-15 12:40:06 +0100

    White space issue

--- template-shop.php
+++ template-shop.php
@@ -27,30 +27,6 @@
             </a>
         </div>
     </div>
-
-    <div class="advert bg-1">
-        <div class="details">
-            <div class="text">
-                <h1>Prayers for Deliverance Bundle</h1>
-                <p>We must exercise our faith to walk in the fullness of His protection. Now is the time to stand firm on God's Word and start protecting yourself and your family today!</p>
-            </div>
-            <a class="button btn-link">
-                <span>Get this today</span>
-            </a>
-        </div>
-    </div>
-
-    <div class="advert bg-2">
-        <div class="details">
-            <div class="text">
-                <h1>Prayers for Deliverance Bundle</h1>
-                <p>We must exercise our faith to walk in the fullness of His protection. Now is the time to stand firm on God's Word and start protecting yourself and your family today!</p>
-            </div>
-            <a class="button btn-link">
-                <span>Get this today</span>
-            </a>
-        </div>
-    </div>
 </section>
 
 <div id="space-50"></div>
