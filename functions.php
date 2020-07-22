fe248182568858e9524468bb26604dd03bc735d1
Signs Madueke <signsmadueke1@gmail.com>
2020-07-22 12:40:23 +0100

    Limited product number

--- functions.php
+++ functions.php
@@ -236,3 +236,41 @@ remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_l
 
 
 
+
+
+
+
+
+
+/**
+ * Change number of related products output
+ */ 
+function woo_related_products_limit() {
+  global $product;
+	
+	$args['posts_per_page'] = 6;
+	return $args;
+}
+add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args', 20 );
+  function jk_related_products_args( $args ) {
+	$args['posts_per_page'] = 3; // 3 related products
+	$args['columns'] = 3; // arranged in 3 columns
+	return $args;
+}
+
+
+
+
+
+
+/**
+ * Change number of upsells output
+ */
+add_filter( 'woocommerce_upsell_display_args', 'wc_change_number_related_products', 20 );
+
+function wc_change_number_related_products( $args ) {
+ 
+ $args['posts_per_page'] = 1;
+ $args['columns'] = 3; //change number of upsells here
+ return $args;
+}
