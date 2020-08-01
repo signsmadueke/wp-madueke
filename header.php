f38aaa6409c9a96b48f0605705612582d52bea34
Signs Madueke <signsmadueke1@gmail.com>
2020-08-01 13:37:45 +0100

    ...

--- header.php
+++ header.php
@@ -5,17 +5,6 @@
 	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 	<?php wp_head(); ?>
-
-        
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131093422-2"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        
-        gtag('config', 'UA-131093422-2');
-    </script>
 </head>
 
 <body <?php body_class('container' . $extraBodyClasses);?>>
