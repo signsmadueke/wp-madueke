12bb247daa516d734ae73852208cc0ebf66996b8
Signs Madueke <signs@Signss-MacBook-Pro.local>
2020-08-15 11:28:23 +0100

    ...

--- header.php
+++ header.php
@@ -161,11 +161,6 @@
                     <img class="svg" src="<?php bloginfo('template_directory');?>/assets/images/icons/social/twitter.svg'; ?>" height="15px" alt="Twitter">
                 </a>
             </li>
-            <li>
-                <a href="https://amazon.com/author/prayermadueke">
-                    <img class="svg" src="<?php bloginfo('template_directory');?>/assets/images/icons/social/amazon.svg'; ?>" height="15px" alt="Amazon">
-                </a>
-            </li>
         </ul>
     </div>
 
